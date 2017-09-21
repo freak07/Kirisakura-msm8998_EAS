BayLibre/android_kernel_common

sched/fair: consider task's boost in find_new_capacity()
In systems where multiple CPUs shares the same frequency domain, a
boosted task on a CPU bias the minimum capacity for all the SG.
Currently, while computing the capacity of a SG we do not consider the
minimum capacity enforced by a boosted tasks.

Make sure that the minimum capacity reported by find_min_capacity
is compliant with the minimum capacity required by the current task.
Indeed, even if the task should remain alone in that SG, the minimum
capacity required by its boosting value should be granted.

Change-Id: I497743da84e1f5f0435f2d896b0d419583139720
Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

kernel/sched/fair.c
@@ -5358,6 +5358,44 @@ long group_norm_util(struct energy_env *eenv)
 	return util_sum;
 }
 
+#ifdef CONFIG_SCHED_TUNE
+static inline unsigned long boosted_task_util(struct task_struct *task);
+
+static inline int
+find_min_capacity(struct energy_env *eenv)
+{
+	const struct sched_group_energy const *sge = eenv->sg->sge;
+	unsigned long min_capacity, cur_capacity;
+	int min_cap_idx, cap_idx;
+	unsigned long min_util;
+
+	/* Non boosted tasks do not affect the minimum capacity */
+	if (!schedtune_task_boost(eenv->task))
+		return eenv->cap_idx;
+
+	/* Find minimum capacity to satify the task boost value */
+	min_util = boosted_task_util(eenv->task);
+	for (min_cap_idx = 0; min_cap_idx < (sge->nr_cap_states-1); min_cap_idx++) {
+		if (sge->cap_states[min_cap_idx].cap >= min_util)
+			break;
+	}
+	min_capacity = sge->cap_states[min_cap_idx].cap;
+
+	/* The current capacity is the one computed by the caller */
+	cur_capacity = sge->cap_states[eenv->cap_idx].cap;
+
+	/*
+	 * Compute the minumum CPU capacity required to support task boosting
+	 * within this SG.
+	 */
+	cap_idx = max(eenv->cap_idx, min_cap_idx);
+
+	return cap_idx;
+}
+#else
+#define find_min_capacity(eenv) eenv->cap_idx
+#endif /* CONFIG_SCHED_TUNE */
+
 static int find_new_capacity(struct energy_env *eenv)
 {
 	const struct sched_group_energy const *sge = eenv->sg->sge;
@@ -5370,6 +5408,9 @@ static int find_new_capacity(struct energy_env *eenv)
 	}
 	/* Keep track of SG's capacity index */
 	eenv->cap_idx = idx;
+
+	/* Update SG's capacity based on boost value of the current task */
+	eenv->cap_idx = find_min_capacity(eenv);
 
 	return eenv->cap_idx;
 }

freak07/Kirisakura-msm8998_EAS

boosted task on a CPU bias the minimum capacity for all the CPUs in the
same SG. Currently, while computing the capacity of a SG we do not
consider the minimum capacity required by a boosted tasks.
Change-Id: Ia89da70ff9c0336d53b77e78898b7f67a5992be7
Signed-off-by: Chris Redpath <chris.redpath@arm.com>
(cherry picked from commit 682b0c8f81ee61ca593ca5eaf74fd707c2cbb12d)
(cherry picked from commit 3996e4ae28de647cc59b81507b2645736b92335b)

kernel/sched/fair.c
@@ -5600,6 +5600,40 @@ long group_norm_util(struct energy_env *eenv, int cpu_idx)
 	return min_t(unsigned long, util_sum, SCHED_CAPACITY_SCALE);
 }
 
+static inline unsigned long boosted_task_util(struct task_struct *p);
+#ifdef CONFIG_SCHED_TUNE
+
+static inline int
+clamp_new_capacity(struct energy_env *eenv, int cap_idx)
+{
+	const struct sched_group_energy *sge = eenv->sg->sge;
+	unsigned long min_capacity, cur_capacity;
+	unsigned long min_util;
+	int min_cap_idx = 0;
+
+	/* Non boosted tasks do not affect the minimum capacity */
+	if (!schedtune_task_boost(eenv->p))
+		return cap_idx;
+
+	/* Find minimum capacity to satify the task boost value */
+	min_util = boosted_task_util(eenv->p);
+	for ( ; min_cap_idx < (sge->nr_cap_states - 1); min_cap_idx++) {
+		if (sge->cap_states[min_cap_idx].cap >= min_util)
+			break;
+	}
+	min_capacity = sge->cap_states[min_cap_idx].cap;
+
+	/* The current capacity is the one computed by the caller */
+	cur_capacity = sge->cap_states[cap_idx].cap;
+
+	/* Min SG's capacity required to support task boosting */
+	return max(min_cap_idx, cap_idx);
+}
+#else
+#define find_min_capacity(eenv) eenv->cap_idx
+#define clamp_new_capacity(eenv, cap_idx) cap_idx
+#endif /* CONFIG_SCHED_TUNE */
+
 static int find_new_capacity(struct energy_env *eenv, int cpu_idx)
 {
 	const struct sched_group_energy *sge = eenv->sg->sge;
@@ -5610,6 +5644,9 @@ static int find_new_capacity(struct energy_env *eenv, int cpu_idx)
 		if (sge->cap_states[idx].cap >= util)
 			break;
 	}
+
+	/* Clamp SG's capacity based on boost value of the current task */
+	idx = clamp_new_capacity(eenv, idx);
 
 	/* Keep track of SG's capacity */
 	eenv->cpu[cpu_idx].cap = sge->cap_states[idx].cap;
@@ -6016,8 +6053,6 @@ static inline unsigned long task_util_est(struct task_struct *p)
 
 	return util_est(sa, UTIL_EST_POLICY);
 }
-
-static inline unsigned long boosted_task_util(struct task_struct *p);
 
 static inline bool __task_fits(struct task_struct *p, int cpu, int util)
 {